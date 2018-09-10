7154ebadb944011b86e9ff9f1fa7e6b9cc2565ea
José Henrique Felipetto <jose.felipetto@pucpr.br>
2018-09-10 11:33:45 -0300

    Adicionado funções para carregar opcodes e regcodes

--- src/assemble.py
+++ src/assemble.py
@@ -1,16 +1,43 @@
 import sys
 import file
 
-def loadOpCode():
-    print("Load Op Code")
+def loadOpCode(filename):
 
-def loadRegCode():
-    print("Load Reg Code")
+    opCodeDict = {}
+
+    with open(filename,"r") as opCodeFile:
+        for line in opCodeFile:
+            splitted = line.split("|")
+            for split in splitted:
+                opCodeDict[split[0]] = split[1]
+
+    print("Opcodes loaded. \n")
+
+    return opCodeDict
+
+def loadRegCode(filename):
+
+    regCodeDict = {}
+
+    with open(filename,"r") as regCodeFile:
+        for line in regCodeFile:
+            splitted = line.split("|")
+            for split in splitted:
+                regCodeDict[split[0]] = split[1]
+
+    print("Regcodes loaded. \n")
+
+    return regCodeDict
 
 def decode(line):
     return
 
 def main():
+
+    opcodes = loadOpCode(sys.argv[1])
+
+    regcodes = loadRegCode(sys.argv[2])
+
     with open(sys.argv[0],"r") as inputFile:
         for line in inputFile:
             opcode = decode(line)
